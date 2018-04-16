9b8ccfc1ac7cbe7dfaf1a0ca02783fed65f7012b
guiadocampus <guiadocampus@gmail.com>
2018-04-16 13:41:17 -0300

    enviando template v0.0.5

---
+++ docs/javascripts/main.js
@@ -0,0 +1,10 @@
+
+function abrir() {
+    document.getElementById("menu").style.right = "0";
+    document.getElementById("menu").style.transition = "all .2s ease";
+}
+
+function fechar() {
+    document.getElementById("menu").style.right = "-100%";
+    document.getElementById("menu").style.transition = "all .2s ease";
+}
