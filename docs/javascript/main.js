d6ce6ca14bbcb1f56dccc1bef8857581f3bed8b4
guiadocampus <guiadocampus@gmail.com>
2018-08-23 14:04:00 -0300

    enviando blog simples

--- docs/javascript/main.js
+++
@@ -1,9 +0,0 @@
-function abrir(){
-    document.getElementById("menu").style.right = "0vw ";
-    document.getElementById("menu").style.transition = "all .2s linear";
-}
-function fechar(){
-    document.getElementById("menu").style.right = "-100vw";
-    document.getElementById("menu").style.transition = "all .2s linear";
-}
-
---
+++ docs/js/main.js
@@ -0,0 +1,9 @@
+function abrir(){
+    document.getElementById("menu").style.right = "0";
+    document.getElementById("menu").style.transition = "all .2s ease";
+}
+
+function fechar(){
+    document.getElementById("menu").style.right = "-110vw";
+    document.getElementById("menu").style.transition = "all .2s ease";
+}
