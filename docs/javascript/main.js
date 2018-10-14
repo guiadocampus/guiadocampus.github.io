62141e4cf95bfc8ddfbe4a278525602e4b4c8e5a
guiadocampus <guiadocampus@gmail.com>
2018-10-14 09:07:23 -0300

    Enviando blog limpo

---
+++ docs/javascript/main.js
@@ -0,0 +1,9 @@
+function abrir(){
+    document.getElementById("menu").style.right = "0vw ";
+    document.getElementById("menu").style.transition = "all .2s linear";
+}
+function fechar(){
+    document.getElementById("menu").style.right = "-100vw";
+    document.getElementById("menu").style.transition = "all .2s linear";
+}
+
